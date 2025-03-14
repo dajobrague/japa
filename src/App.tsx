7e9374b79d0cb69533b5c70ac6f6bdd8e4804178
David Bracho <brachod@gmail.com>
2025-03-14 09:45:36 -0600

    Fix: Switch to HashRouter and add base URL for production deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import DemoFormProvider from "./contexts/DemoFormContext";
 import Index from "./pages/Index";
 import Solutions from "./pages/Solutions";
@@ -21,7 +21,7 @@ const App = () => (
       <DemoFormProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/solutions" element={<Solutions />} />
@@ -33,7 +33,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </DemoFormProvider>
     </TooltipProvider>
   </QueryClientProvider>
